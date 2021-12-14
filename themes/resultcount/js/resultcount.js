0cd2bdcdd0a711a57f1db808403c1652cda25a18
Hajo Seng <hajoseng@sub.uni-hamburg.de>
2021-12-14 10:23:45 +0100

    minor Updates

--- themes/resultcount/js/resultcount.js
+++ themes/resultcount/js/resultcount.js
@@ -1,20 +1,24 @@
-$(document).ready(function() {
-    /*
-     * Get result count for inactive Tabs
-     */
+/* global VuFind */
+
+VuFind.register('resultcount', function resultCount() {
+  function init() {
     $('ul.nav-tabs li a').each(function(){
-        var $this = $(this);
-        if ($this.attr('href') !== undefined) {
-            var queryString = $this.attr('href').replace(/^.*\?/, '');
-            var source = $this.data('source');
-            jQuery.ajax({
-                url:VuFind.path + '/AJAX/JSON?method=getResultCount',
-                dataType:'json',
-                data:{querystring:encodeURIComponent(queryString), source:source},
-                success:function(data, textStatus){
-                    $this.append(' (' + data.data.total.toLocaleString() + ')')
-                }
-            });
-        }
+      var $this = $(this);
+      if ($this.attr('href') !== undefined) {
+        var queryString = $this.attr('href').replace(/^.*\?/, '');
+        var source = $this.data('source');
+        jQuery.ajax({
+          url:VuFind.path + '/AJAX/JSON?method=getResultCount',
+          dataType:'json',
+          data:{querystring:encodeURIComponent(queryString), source:source},
+          success:function(data, textStatus){
+            $this.append(' (' + data.data.total.toLocaleString() + ')')
+          }
+        });
+      }
     });
+  }
+  return {
+    init: init
+  };
 });
