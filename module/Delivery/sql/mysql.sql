adb0c2c564fa31fa9a18f07610ec5588e9322bb0
Hajo Seng <hajoseng@sub.uni-hamburg.de>
2022-02-08 13:49:30 +0100

    ohne DROP TABLE

--- module/Delivery/sql/mysql.sql
+++ module/Delivery/sql/mysql.sql
@@ -15,7 +15,6 @@
 
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-DROP TABLE IF EXISTS `user_delivery`;
 CREATE TABLE IF NOT EXISTS `user_delivery` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) NOT NULL DEFAULT '0',
@@ -27,7 +26,6 @@ CREATE TABLE IF NOT EXISTS `user_delivery` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
-DROP TABLE IF EXISTS `delivery`;
 CREATE TABLE IF NOT EXISTS `delivery` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `user_delivery_id` int(11) NOT NULL DEFAULT '0',
